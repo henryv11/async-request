1c5aa74e8cbf775e2fbe4764d64276e0d8a507a3
Henry <henry@coolbet.com>
2021-06-01 14:24:14 +0300

    ehh .... idk

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,49 @@
 import http from 'http';
 import https from 'https';
-import { pipeline } from 'stream';
-
-const parserByContentType = {
-  'application/json': JSON.parse,
-};
+import stream from 'stream';
+import path from 'path';
 
 const driverByProtocol = {
   'https:': https.request,
   'http:': http.request,
 };
 
-export default function request<T = unknown>({
-  agent,
-  body,
-  encoding = 'utf-8',
-  headers = {},
-  json,
-  method = 'GET',
-  query = {},
-  readableStream,
-  timeout = 30000,
-  url,
-  writableStream,
-}: {
-  agent?: http.Agent | https.Agent | boolean;
-  body?: unknown;
-  encoding?: BufferEncoding;
-  headers?: http.OutgoingHttpHeaders;
-  json?: unknown;
-  method?: string;
-  query?: Record<string, string | number | (string | number)[]>;
-  readableStream?: NodeJS.ReadableStream;
-  timeout?: number;
-  url: string;
-  writableStream?: NodeJS.WritableStream;
-}) {
-  const { protocol, host, port, pathname: path, searchParams: baseSearchParams } = new URL(url);
+export default function request<T = unknown>(
+  url: string,
+  {
+    agent,
+    body,
+    encoding = 'utf-8',
+    headers = {},
+    json,
+    method = 'GET',
+    path: pathArgument = '',
+    query = {},
+    response: responseParser = _ => <T>(<unknown>_),
+    timeout = 30000,
+  }: {
+    agent?: http.Agent | https.Agent | boolean;
+    body?: string | ArrayBuffer | stream.Readable;
+    encoding?: BufferEncoding;
+    headers?: http.OutgoingHttpHeaders;
+    json?: unknown;
+    method?: 'GET' | 'POST' | 'PUT' | 'DELETE';
+    path?: string;
+    query?: Record<string, string | number | (string | number)[]>;
+    response?: (rawBody: string) => T | stream.Writable;
+    timeout?: number;
+    writeStream?: NodeJS.WritableStream;
+  } = {},
+) {
+  const {
+    protocol,
+    host,
+    port,
+    pathname: pathURL,
+    searchParams: searchParamsURL,
+  } = new URL(url.startsWith('http') ? url : 'http://' + url);
   const driver = driverByProtocol[<keyof typeof driverByProtocol>protocol];
-  const searchParams = new URLSearchParams(baseSearchParams);
+  const searchParams = new URLSearchParams(searchParamsURL);
   const response = {
     code: -1,
     message: '',
@@ -54,66 +59,60 @@ export default function request<T = unknown>({
   });
   headers['accept'] = ['application/json', 'text/*'];
   headers['accept-charset'] = encoding;
+  if (json) {
+    body = JSON.stringify(json);
+    headers['content-type'] = 'application/json;charset=' + encoding;
+  }
   return new Promise<typeof response>((resolve, reject) => {
-    if (json) {
-      body = JSON.stringify(json);
-      headers['content-type'] = 'application/json';
-      headers['content-length'] = (<string>body).length;
-    }
-    const req = driver({ host, port, path, method, headers, searchParams, agent }, res => {
-      response.code = res.statusCode || -1;
-      response.message = res.statusMessage || '';
-      response.headers = res.headers;
-      res.setEncoding(encoding).on('error', error => {
-        res.destroy(error);
-        reject(error);
-      });
-      if (writableStream) {
-        return pipeline(res, writableStream, error => {
-          if (error) {
-            res.destroy(error);
-            return reject(error);
-          }
-          resolve(response);
-        });
-      }
-      const contentType = res.headers['content-type']?.split(';')[0];
-      const parser = <(val: unknown) => T>(
-        (contentType && contentType in parserByContentType
-          ? parserByContentType[<keyof typeof parserByContentType>contentType]
-          : _ => _)
-      );
-      res
-        .on('data', data => {
-          response.body += data;
-        })
-        .on('end', () => {
-          try {
-            response.body = parser(response.body);
-            resolve(response);
-          } catch (error) {
-            reject(error);
-          }
-        });
-    })
-      .setTimeout(timeout)
-      .on('timeout', () => reject(new Error('request timed out')))
-      .on('error', error => {
-        req.destroy(error);
-        reject(error);
-      });
-    if (readableStream) {
-      return pipeline(readableStream, req, error => {
-        if (error) {
-          req.destroy(error);
-          return reject(error);
+    const req = driver(
+      { host, port, path: path.join(pathURL, pathArgument), method, headers, searchParams, agent },
+      res => {
+        function onError(error: Error) {
+          res.destroy(error);
+          reject(error);
         }
-        req.end();
-      });
+        res.on('error', onError);
+        response.code = res.statusCode || -1;
+        response.message = res.statusMessage || '';
+        response.headers = res.headers;
+        if (responseParser instanceof stream.Writable) {
+          stream.pipeline(res, responseParser, error => (error ? onError(error) : resolve(response)));
+        } else {
+          const buffer: Uint8Array[] = [];
+          res.on('data', data => {
+            buffer.push(data);
+          });
+          res.on('end', () => {
+            try {
+              response.body = (<(_: string) => T>responseParser)(Buffer.concat(buffer).toString(encoding));
+              resolve(response);
+            } catch (error) {
+              reject(error);
+            }
+          });
+        }
+      },
+    );
+    function onError(error: Error) {
+      req.destroy(error);
+      reject(error);
     }
-    if (body) {
-      req.write(body, encoding, err => err && reject(err));
+    req.setTimeout(timeout);
+    req.on('timeout', () => onError(new Error('request timed out after ' + timeout + ' ms')));
+    req.on('error', onError);
+    if (method === 'GET') {
+      req.end();
+    } else if (body) {
+      if (body instanceof stream.Readable) {
+        stream.pipeline(body, req, error => (error ? onError(error) : req.end()));
+      } else {
+        if (!req.getHeader('content-length')) {
+          req.setHeader('content-length', Buffer.byteLength(<string | ArrayBuffer>body));
+        }
+        req.write(body, encoding, error => (error ? onError(error) : req.end()));
+      }
+    } else {
+      req.end();
     }
-    req.end();
   });
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,28 +1,52 @@
-import fs from 'fs';
 import request from '../src';
-describe('node-request', () => {
-  test('works', async () => {
-    interface ResponseBody {
-      status: string;
-      data: {
-        id: number;
-        employee_name: string;
-        employee_salay: number;
-        employee_age: number;
-        profile_image: string;
-      }[];
-    }
-    const response = await request<ResponseBody>({
-      url: 'http://dummy.restapiexample.com/api/v1/employees',
-    });
-    expect(response.body.data[0]).toBeDefined();
-  });
+import nock from 'nock';
 
-  test('stream response', async () => {
-    const filePath = __dirname + '/response.txt';
-    const writableStream = fs.createWriteStream(filePath);
-    await request({ url: 'http://dummy.restapiexample.com/api/v1/employees', writableStream });
-    expect(fs.existsSync(filePath)).toBe(true);
-    fs.unlinkSync(filePath);
+nock.recorder.rec({ logging: console.log });
+
+describe('node-request', () => {
+  // test('works', async () => {
+  //   interface ResponseBody {
+  //     status: string;
+  //     data: {
+  //       id: number;
+  //       employee_name: string;
+  //       employee_salay: number;
+  //       employee_age: number;
+  //       profile_image: string;
+  //     }[];
+  //   }
+  //   const response = await request<ResponseBody>({
+  //     url: 'http://dummy.restapiexample.com/api/v1/employees',
+  //   });
+  //   expect(response.body.data[0]).toBeDefined();
+  // });
+  // test('stream response', async () => {
+  //   const filePath = __dirname + '/response.txt';
+  //   const writableStream = fs.createWriteStream(filePath);
+  //   await request({ url: 'http://dummy.restapiexample.com/api/v1/employees', writeStream: writableStream });
+  //   expect(fs.existsSync(filePath)).toBe(true);
+  //   fs.unlinkSync(filePath);
+  // });
+
+  // test('fff', async () => {
+  //   const url = 'http://url.com';
+  //   nock(url).post('/hello').reply(200, {});
+
+  //   const readStream = fs.createReadStream(__dirname + '/test_file.txt', 'utf-8');
+
+  //   const response = await request(url + '/hello', {
+  //     method: 'POST',
+  //     body: readStream,
+  //   });
+
+  //   console.log(response);
+  // });
+
+  test.only('bbb', async () => {
+    const url = 'http://test_url.com/';
+    const path = '/path';
+    nock(url).get(path).reply(200, 'hello');
+    const response = await request(url, { path });
+    console.log(response.body);
   });
 });
